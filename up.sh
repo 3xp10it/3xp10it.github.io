3af75c59304b782cde621470802fe5e23045aa5d
3xp10it <quanyechavshuo@gmail.com>
2024-10-28 12:26:26 +0800

    update

--- up.sh
+++ up.sh
@@ -1,3 +1,6 @@
+if [ -f ~/.bashrc ]; then
+    . ~/.bashrc
+fi
 if [ -d /root/myblog ] && [ -d /root/3xp10it.github.io ]
 then
     cd /root/myblog && pkill jekyll;sleep 3 && cp index.html tmp.html && cp index_bak.html index.html && sh -c "rm -r _site/ & jekyll serve --watch & sleep 3 && exit" && cp _site/index.html index2.html && cp tmp.html index.html && echo congratulations! commands execute ok to here!
